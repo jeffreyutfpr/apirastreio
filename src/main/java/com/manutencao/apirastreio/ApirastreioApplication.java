02fc5f2a24762c0982cf80cd237fcb76f70ede0b
Jeffrey Souza <jeffrey@alunos.utfpr.edu.br>
2023-11-12 17:40:39 -0300

    init project

---
+++ src/main/java/com/manutencao/apirastreio/ApirastreioApplication.java
@@ -0,0 +1,13 @@
+package com.manutencao.apirastreio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirastreioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirastreioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manutencao/apirastreio/ApirastreioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manutencao.apirastreio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirastreioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
